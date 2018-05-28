c8da4126f37044c18b14c3c4d1f4bbb661e69cea
Hazem El-Sisy <hazem@easternpencil.com>
2018-05-28 15:36:11 +0200

    no message

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -27,36 +27,38 @@ export const refreshPlayerId=(user_id,token_id)=>dispatch=>{
   }
 }
 
-export const loginUser=(user,context)=>dispatch=>{
-  var base_url =new Base()
-  var LOGIN_URL="http://" + base_url.baseUrl + "login"
-  try {
-    axios
-      .post(LOGIN_URL, user)
-      .then(async(user)=> {
-        if (user && user.phone !="") {
-          context.setState({
-            loading: false,
-            VerifyPhoneScreen: "VerifyPhoneScreen"
-          });
-          context.props.navigation.navigate("VerifyPhoneScreen", user);
-          await AsyncStorage.setItem('user','dddddddddddd')
-          dispatch({
-            type:LOGIN,
-            payload:user
-          })
-        } else {
+export const loginUser=(user,context)=>{
+  return(dispatch) => {
+    var base_url =new Base()
+    var LOGIN_URL="http://" + base_url.baseUrl + "login"
+    try {
+      axios
+        .post(LOGIN_URL, user)
+        .then(async(user)=> {
+          if (user && user.phone !="") {
+            context.setState({
+              loading: false,
+              VerifyPhoneScreen: "VerifyPhoneScreen"
+            });
+            context.props.navigation.navigate("VerifyPhoneScreen", user);
+            await AsyncStorage.setItem('user','dddddddddddd')
+            dispatch({
+              type:LOGIN,
+              payload:user
+            })
+          } else {
+            context.setState({ loading: false });
+          }
+        })
+        .catch(function(error) {
           context.setState({ loading: false });
-        }
-      })
-      .catch(function(error) {
-        context.setState({ loading: false });
-        Toast.show("يرجي التاكد من البيانات", Toast.LONG);
-        console.log(error);
-      });
-  }catch (error) {
-    Toast.show("", Toast.LONG);
-    context.setState({ loading: false });
-    console.error(error);
+          Toast.show("يرجي التاكد من البيانات", Toast.LONG);
+          console.log(error);
+        });
+    }catch (error) {
+      Toast.show("", Toast.LONG);
+      context.setState({ loading: false });
+      console.error(error);
+    }
   }
 }
