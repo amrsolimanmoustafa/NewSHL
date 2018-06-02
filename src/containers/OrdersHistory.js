9cdc71a69a63dd57964396b90cfcb29ff0cec598
HeshamEG <heshamsalama1212901022@gmail.com>
2018-06-02 15:24:02 +0200

    no msg

--- src/containers/OrdersHistory.js
+++ src/containers/OrdersHistory.js
@@ -10,6 +10,10 @@ import {
 import masterStyle from './masterStyle';
 import Container from './Components/container';
 import { Images } from './../Themes';
+import Base from '../Base';
+import axios from 'axios'
+import { connect } from 'react-redux'
+import { withNavigation } from "react-navigation";
 
 // style //
 const {
@@ -17,60 +21,83 @@ const {
   appGrayColor,
 } = masterStyle;
 
-export default class OrdersHistory extends Component {
+ class OrdersHistory extends Component {
   state = {
     ordersHistoryList: 
-    [
-      { id: 1, 
-        name: 'ماجد أحمد', 
-        image: Images.offer1,
-        cost: '100 ريال', 
-        status: 'مكتمل',
-        statusLabel: 'لحظى',
-        serviceNmae: 'خدمة الصرف الصحي', 
-        serviceType: 'خدمة فرعية 1', 
-        cityName: 'حى الزهور', 
-        address: '193 الحى السابع بجوار مسجد نورى الخطاب',
-        date: '12/3/2018',
-      },
-      { id: 2, 
-        name: 'ماجد أحمد', 
-        image: Images.humanIcon,
-        cost: '100 ريال', 
-        status: 'مكتمل',
-        statusLabel: 'لحظى',
-        serviceNmae: 'خدمة الصرف الصحي', 
-        serviceType: 'خدمة فرعية 1', 
-        cityName: 'حى الزهور', 
-        address: '193 الحى السابع بجوار مسجد نورى الخطاب',
-        date: '12/3/2018',
-      },
-      { id: 3, 
-        name: 'عبدالكريم أحمد', 
-        image: Images.humanIcon,
-        cost: '100 ريال', 
-        status: 'غير مكتمل',
-        statusLabel: 'مجدول',
-        serviceNmae: 'خدمة الصرف الصحي', 
-        serviceType: 'خدمة فرعية 1', 
-        cityName: 'حى الزهور', 
-        address: '193 الحى السابع بجوار مسجد نورى الخطاب',
-        date: '12/3/2018',
-      },
-      { id: 4, 
-        name: 'ماجد أحمد', 
-        image: Images.humanIcon,
-        cost: '100 ريال', 
-        status: 'مكتمل',
-        statusLabel: 'لحظى',
-        serviceNmae: 'خدمة الصرف الصحي', 
-        serviceType: 'خدمة فرعية 1', 
-        cityName: 'حى الزهور', 
-        address: '193 الحى السابع بجوار مسجد نورى الخطاب',
-        date: '12/3/2018',
-      },
-    ],
+    [  ]
   }
+async  componentWillMount(){
+   await this.getHistoryList
+  }
+  getHistoryList=()=>{
+
+    var base_url =new Base()
+    console.log('test')
+
+
+    var ORDERS_HISTORY_URL="http://" + base_url.baseUrl + "clintordershoistry/"+this.props.user_id+"?lang="+base_url.lang  
+  try {
+    axios
+      .get(ORDERS_HISTORY_URL)
+      .then((res) =>{
+ console.log(res)
+      })
+      .catch(function(error) {
+      
+      });
+  }catch (error) {
+  
+  }
+  }
+      // { id: 1, 
+      //   name: 'ماجد أحمد', 
+      //   image: Images.offer1,
+      //   cost: '100 ريال', 
+      //   status: 'مكتمل',
+      //   statusLabel: 'لحظى',
+      //   serviceNmae: 'خدمة الصرف الصحي', 
+      //   serviceType: 'خدمة فرعية 1', 
+      //   cityName: 'حى الزهور', 
+      //   address: '193 الحى السابع بجوار مسجد نورى الخطاب',
+      //   date: '12/3/2018',
+      // },
+      // { id: 2, 
+      //   name: 'ماجد أحمد', 
+      //   image: Images.humanIcon,
+        // cost: '100 ريال', 
+        // status: 'مكتمل',
+      //   statusLabel: 'لحظى',
+      //   serviceNmae: 'خدمة الصرف الصحي', 
+      //   serviceType: 'خدمة فرعية 1', 
+      //   cityName: 'حى الزهور', 
+      //   address: '193 الحى السابع بجوار مسجد نورى الخطاب',
+      //   date: '12/3/2018',
+      // },
+      // { id: 3, 
+      //   name: 'عبدالكريم أحمد', 
+      //   image: Images.humanIcon,
+      //   cost: '100 ريال', 
+      //   status: 'غير مكتمل',
+      //   statusLabel: 'مجدول',
+      //   serviceNmae: 'خدمة الصرف الصحي', 
+      //   serviceType: 'خدمة فرعية 1', 
+      //   cityName: 'حى الزهور', 
+      //   address: '193 الحى السابع بجوار مسجد نورى الخطاب',
+      //   date: '12/3/2018',
+      // },
+      // { id: 4, 
+      //   name: 'ماجد أحمد', 
+      //   image: Images.humanIcon,
+      //   cost: '100 ريال', 
+      //   status: 'مكتمل',
+      //   statusLabel: 'لحظى',
+      //   serviceNmae: 'خدمة الصرف الصحي', 
+      //   serviceType: 'خدمة فرعية 1', 
+      //   cityName: 'حى الزهور', 
+      //   address: '193 الحى السابع بجوار مسجد نورى الخطاب',
+      //   date: '12/3/2018',
+      // },
+  
   ///////////////////////////////////////////
   renderOrderItem = ({ item }) => {
     return (
@@ -183,3 +210,24 @@ const styles = {
   }
 
 };
+
+const mapStateToProps = state => {
+  return {
+    common: state.common,
+    user_id: state.auth.user_id,
+    makeOrder: state.makeOrder,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps, {
+  // getServices,
+  // refreshPlayerId,
+  // reverseCoordinatesToAdress,
+  // setCoordnates
+}) (withNavigation(OrdersHistory))
